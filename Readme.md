e2fe9ea636cddc4fa56c1fca7fa2e7fe1f8cf8e6
atulit <coderatulit@gmail.com>
2021-08-02 00:25:54 +0530

    quick fixes for warnings

--- Readme.md
+++ Readme.md
@@ -1,1 +1,18 @@
 # crypto-app-v2
+
+## API
+
+Initiating command: `yarn start` or `npm start` inside the `./api` directory
+PORT: localhost:5050
+
+### URLs
+
+- `/api/stats/all` : for all coins
+- `/api/stats/[id]` for query search
+- `/api/stats/coin/[id]` for specific id name like : BITCOIN instead of a regular query search
+
+---
+
+## React App
+
+Exactly same as before.
--- crypto-app/src/Components/CoinsListContainer.jsx
+++ crypto-app/src/Components/CoinsListContainer.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { HOST_ADDRESS } from "../envVars";
 import CardList from "./CardList";
+// import "dotenv/config";
+require("dotenv").config();
 
 function CoinsListContainer({ props }) {
   const [coinsList, setCoinsList] = useState([]);
-  useEffect(async () => {
-    const query = props.query || "all";
-    const res = await fetch(`${HOST_ADDRESS}/api/stats/${query}`);
-    // const res = await fetch(`${HOST_ADDRESS}/api/stats/bitcoin`);
-    // console.log(`${HOST_ADDRESS}/api/stats/${query}`);
-    const json = await res.json();
-    setCoinsList(json);
+  useEffect(() => {
+    (async () => {
+      const query = props.query || "all";
+      const res = await fetch(`${HOST_ADDRESS}/api/stats/${query}`);
+      console.log(process.env);
+      // const res = await fetch(`${HOST_ADDRESS}/api/stats/bitcoin`);
+      // console.log(`${HOST_ADDRESS}/api/stats/${query}`);
+      const json = await res.json();
+      setCoinsList(json);
+    })();
   }, [props.query]);
   return (
     <>
