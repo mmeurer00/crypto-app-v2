ea3c0d4e9b9f749c89ca9d0d75c9a3ec497b37fa
Maxine <maxinemeurer@yahoo.com>
2021-08-04 12:24:14 -0400

    coin styling

--- crypto-app/src/Components/Card.jsx
+++ crypto-app/src/Components/Card.jsx
@@ -14,9 +14,9 @@ function Card({ props: coin }) {
           <p className="coin_symbol">{coin.symbol}</p>
           <div className="coin_data">
             <p className="coin_price">{"$" + coin.current_price}</p>
-            <p>{"$" + coin.fully_diluted_valuation}</p>
-            <p>{coin.ath_change_percentage}</p>
-            <p>{coin.market_cap}</p>
+            <p className="coin_volume">{"$" + coin.fully_diluted_valuation}</p>
+            <p className="coin_percent">{coin.ath_change_percentage}</p>
+            <p className="coin_marketcap">{coin.market_cap}</p>
           </div>
         </div>
       </div>
