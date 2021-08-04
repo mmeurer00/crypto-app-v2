7f3820b13c074bbdf9d83d7ffe1475fdfb8acd98
Maxine <maxinemeurer@yahoo.com>
2021-08-04 11:38:21 -0400

    add user token retrieve

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -7,9 +7,29 @@ import Preferences from './Components/Preferences/Preferences';
 import SearchWindow from "./Components/SearchWindow";
 import './App.css'
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  /* You need to use ?.
+  —when accessing the token property because 
+  when you first access the application, the value of 
+  sessionStorage.getItem('token') will be undefined. 
+  If you try to access a property, you will generate an error. */
+  return userToken?.token
+}
+
+/* As of right now, until logout is added, you can
+clear out the token by either deleting the token in 
+the Storage tab in your developer tools or by typing
+sessionStorage.clear() in your developer console. */
+
 function App() {
-  
-  const [token, setToken] = useState();
+
+  const token = getToken();
 
   if(!token) {
     return <Login setToken={setToken} />
