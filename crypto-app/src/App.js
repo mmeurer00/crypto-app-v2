1fa8af25cbd837182ebf66572d31dda7be55d0f7
Maxine <maxinemeurer@yahoo.com>
2021-08-04 11:08:06 -0400

    add prefereces, dashboard, routes, login page

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,9 +1,35 @@
 // import API from "./Components/FunkyExperimentLab/API";
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Preferences from './Components/Preferences/Preferences';
 import SearchWindow from "./Components/SearchWindow";
+import './App.css'
 
 function App() {
+  
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <>
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
       <SearchWindow />
     </>
   );
---
+++ crypto-app/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Dashboard() {
+  return(
+    <h2>Dashboard</h2>
+  );
+}
+export default Dashboard;
