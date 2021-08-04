faa791ef1fce57d0a6d4eb847eb192b429bf7747
Maxine <maxinemeurer@yahoo.com>
2021-08-04 11:49:53 -0400

    add in local storage for users

---
+++ crypto-app/src/App/App.js
@@ -0,0 +1,39 @@
+// import API from "./Components/FunkyExperimentLab/API";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from '../Components/Login/Login';
+import Dashboard from '../Components/Dashboard/Dashboard';
+import Preferences from '../Components/Preferences/Preferences';
+import SearchWindow from "../Components/SearchWindow";
+import useToken from './useToken';
+import './App.css'
+
+function App() {
+
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <>
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+      <SearchWindow />
+    </>
+  );
+}
+
+export default App;
---
+++ crypto-app/src/App/useToken.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+function useToken() {
+    const getToken = () => {
+        const tokenString = localStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
+         /* You need to use ?.
+        —when accessing the token property because 
+        when you first access the application, the value of 
+        localStorage.getItem('token') will be undefined. 
+        If you try to access a property, you will generate an error. */
+        return userToken?.token
+    };
+    const [token, setToken] = useState(getToken());
+
+    const saveToken = userToken => {
+        localStorage.setItem('token', JSON.stringify(userToken));
+        setToken(userToken.token);
+    };
+
+    return {
+        setToken: saveToken,
+        token
+    }
+  }
+
+/* As of right now, until logout is added, you can
+clear out the token by either deleting the token in 
+the Storage tab in your developer tools or by typing
+localStorage.clear() in your developer console. */
+
+export default useToken;
