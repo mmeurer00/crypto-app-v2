92d537760ccc131b1a5cad32823d747000d7d0ae
Maxine <maxinemeurer@yahoo.com>
2021-08-04 11:24:17 -0400

    create local login and api for login

--- crypto-app/src/Components/Login/Login.js
+++ crypto-app/src/Components/Login/Login.js
@@ -2,20 +2,41 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
 
-export default function Login({ setToken }) {
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
+}
+
+function Login({ setToken }) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+          username,
+          password
+        });
+        setToken(token);
+}
+
   return(
     <div className="login-wrapper">
         <h1>Please Log In</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
         <label>
             <p>Username</p>
-            <input type="text" />
+            <input type="text" onChange={e => setUserName(e.target.value)}/>
         </label>
         <label>
             <p>Password</p>
-            <input type="password" />
+            <input type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <div>
             <button type="submit">Submit</button>
@@ -28,3 +49,5 @@ export default function Login({ setToken }) {
 Login.propTypes = {
     setToken: PropTypes.func.isRequired
 }
+
+export default Login
