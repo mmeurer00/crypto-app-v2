a4103bdda5ec0734dadcac7759d205a2120df998
Maxine <maxinemeurer@yahoo.com>
2021-08-14 18:06:13 -0400

    login page

--- crypto-app/src/Components/Login/Login.js
+++ crypto-app/src/Components/Login/Login.js
@@ -1,35 +1,55 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
+import axios from 'axios';
 
-async function loginUser(credentials) {
-    return fetch('http://localhost:8080/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
-    })
-      .then(data => data.json())
-}
+// async function loginUser(credentials) {
+//     return fetch('http://localhost:8080/login', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(credentials)
+//     })
+//       .then(data => data.json())
+// }
 
-function Login({ setToken }) {
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
+class Login extends Component {
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const token = await loginUser({
-          username,
-          password
-        });
-        setToken(token);
-}
+  handleSubmit = e => {
+    e.preventDefault();
+    const data = {
+        username: this.username,
+        email: this.email,
+        password: this.password
+    }
+    axios.post('http://localhost:8000/signup', data).then(
+      result => {
+          console.log(result)
+      }
+    ).catch(
+        err => {
+          console.log(err)
+        }
+      )
+  }
+// function Login({ setToken }) {
+//     const [username, setUserName] = useState();
+//     const [password, setPassword] = useState();
 
+//     const handleSubmit = async e => {
+//         e.preventDefault();
+//         const token = await loginUser({
+//           username,
+//           password
+//         });
+//         setToken(token);
+// }
+render(){
   return(
     <div className="login-wrapper">
         <h1>Please Log In</h1>
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
         <label>
             <p>Username</p>
             <input type="text" onChange={e => setUserName(e.target.value)}/>
@@ -41,13 +61,25 @@ function Login({ setToken }) {
         <div>
             <button type="submit">Submit</button>
         </div>
-        </form>
+        </form> */}
+    <form onSubmit={this.handleSubmit}>
+        <label>
+            <p>Username</p>
+        <input type="text" placeholder="Username" onChange={e => this.username = e.target.value}/>
+        </label>
+        <label>
+            <p>Email</p>
+        <input type="email" placeholder="Email" onChange={e => this.email = e.target.value}/>
+        </label>
+        <label>
+            <p>Password</p>
+            <input type="password" placeholder="Password" onChange={e => this.password = e.target.value}/>
+        </label>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
-  )
+  )}
 }
-
-Login.propTypes = {
-    setToken: PropTypes.func.isRequired
-}
-
-export default Login
+  export default Login
--- crypto-app/src/Components/Signup/Signup.js
+++ crypto-app/src/Components/Signup/Signup.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class Signup extends Component {
 
-    handleSubmit = () => {
-        console.log('works')
+    handleSubmit = e => {
+        e.preventDefault();
+        const data ={
+            username: this.username,
+            email: this.email,
+            password: this.password,
+            password_confirm: this.confirmPassword
+        }
+        axios.post('http://localhost:8000/signup', data).then(
+            result => {
+                console.log(result)
+            }
+        ).catch(
+            err => {
+                console.log(err)
+            }
+        )
     }
     render() {
         return (
@@ -11,19 +27,19 @@ class Signup extends Component {
                <form onSubmit={this.handleSubmit}>
                     <label>
                         <p>Username</p>
-                    <input type="text" placeholder="Username" onChange={e => setPassword(e.target.value)}/>
+                    <input type="text" placeholder="Username" onChange={e => this.username = e.target.value}/>
                     </label>
                     <label>
                         <p>Email</p>
-                    <input type="email" placeholder="Email" onChange={e => setPassword(e.target.value)}/>
+                    <input type="email" placeholder="Email" onChange={e => this.email = e.target.value}/>
                     </label>
                     <label>
                         <p>Password</p>
-                        <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                        <input type="password" placeholder="Password" onChange={e => this.password = e.target.value}/>
                     </label>
                     <label>
                         <p>Confirm Password</p>
-                        <input type="password" placeholder="Confirm Password" onChange={e => setPassword(e.target.value)}/>
+                        <input type="password" placeholder="Confirm Password" onChange={e => this.confirmPassword = e.target.value}/>
                     </label>
                     <div>
                         <button type="submit">Sign Up</button>
